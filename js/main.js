3acbfc6c105a1f14e81cada995cc1c07d9b7687c
LintonRobinson <40444726+LintonRobinson@users.noreply.github.com>
2024-12-07 19:40:47 -0500

    Fixed mobile spacing on hero/HIW/WWWU

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+// Transparent to solid nav bar
+
+const header = document.querySelector("header");
+
+window.addEventListener('scroll' , () => {
+    if (window.scrollY > 50) {
+        header.classList.add('headerSolidBackground');
+    } else if (window.scrollY <= 50) {
+        header.classList.remove('headerSolidBackground')
+    }; 
+}
+);
+
+
+
+
+
+
+
+
 
 // FAQ Functionality
 

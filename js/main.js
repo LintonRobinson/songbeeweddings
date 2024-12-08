0e266a27f8f012bea9c66f9e1e373df23cc83f0d
LintonRobinson <40444726+LintonRobinson@users.noreply.github.com>
2024-12-07 20:40:19 -0500

    Fixed nav bar pushing down content, who we are width

--- js/main.js
+++ js/main.js
@@ -74,6 +74,8 @@ document.querySelector('.faq-question-4').addEventListener('click', () => {
 // Hamburger Menu Functionality
 document.querySelector('.hamburger-icon').addEventListener('click',() => {
     document.querySelector('.full-screen-mobile-menu-wrapper').classList.toggle('full-screen-mobile-menu-wrapper-hidden')
+    header.classList.toggle('headerSolidBackground');
+
 })
 
 
